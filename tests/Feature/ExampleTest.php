85a9a24e08fa86e8c802bf944b8fc184ecc99d42
Francisco Barrento <francisco.barrento@gmail.com>
2023-09-22 22:00:11 +0100

    Added first test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,15 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_the_homepage_contains_symfony(): void
+    {
+
+        $response = $this->get('/');
+
+        $response->assertSee('Symfony');
+
+        $response->assertStatus(200);
+
+    }
 }
